336189e7c094b4d4ab861d39f328959ff7659d12
laurasadi <laura.sadinauskaite@gmail.com>
2020-10-28 09:30:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let pazymys = 9;
+
+switch (pazymys) {
+  case 3:
+  console.log ( pazymys+ ' Man gaila tau nepasiseke' );
+  break;
+  case 4:
+  console.log ( pazymys+ ' Man gaila tau nepasiseke' );
+  break;
+  case 5:
+  console.log ( pazymys+ ' Man gaila tau nepasiseke' );
+  break;
+  case 6:
+  console.log ( pazymys+ ' Tu gali geriau' );
+  break;
+  case 7:
+  console.log ( pazymys+ ' Tu gali geriau' );
+  break;
+  case 8:
+  console.log ( pazymys+ ' Jau jau neblogai' );
+  break;
+  case 9:
+  console.log ( pazymys+ ' Jau jau neblogai' );
+  break;
+  case 10:
+  console.log ( pazymys+ 'Jeee tau pavyko' );
+  break;
+}
+// galima ir kelis case i viena isideti pvz case case//
+ // case 3:
+ // case 4:
+//   console.log ( pazymys+ ' Man gaila tau nepasiseke' );
+//   break;
